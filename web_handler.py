26155ead4c6a451065e36f62743df74774e47b9a
Eric Njogu <kunadawa@gmail.com>
2019-11-06 17:00:09 +0300

    refactor: reduce amount of data logged

--- web_handler.py
+++ web_handler.py
@@ -15,5 +15,5 @@ class WebDetectionHandler(detection_handler_pb2_grpc.DetectionHandlerServicer):
         frame = numpy.array(request.frame.numbers).reshape(request.frame.shape)
         # save image to buffer or temporary file location
         self.queue.put(request)
-        logging.info(f'placed request on queue: {request}')
+        logging.info(f'placed request on queue, frame_count: {request.frame_count}')
         return detection_handler_pb2.handle_detection_response(status=True)
