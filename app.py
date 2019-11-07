8ff8386a7e92a7a60a9d541b68d50013e2119d80
Eric Njogu <kunadawa@gmail.com>
2019-11-07 14:31:17 +0300

    refactor: removing test message on the queue

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ if __name__ == '__main__':
     # parse args
     parser = argparse.ArgumentParser(description=" stream detected object events to web clients")
     parser.add_argument("handler_port", help="port to listen for detection handling requests")
-    parser.add_argument("--testing", help="add some test items to the event queue on startup", action="store_true")
     args = parser.parse_args()
     # credit - https://www.semantics3.com/blog/a-simplified-guide-to-grpc-in-python-6c4e25f0c506/
     # create server
@@ -47,18 +46,6 @@ if __name__ == '__main__':
     detection_handler_pb2_grpc.add_DetectionHandlerServicer_to_server(web_handler, server);
     # start grpc server
     server.start()
-    # setup queue
-    if args.testing is True:
-        event_queue.put(detection_handler_pb2.handle_detection_request(
-            start_timestamp=1106,
-            detection_classes=[3],
-            detection_scores=[.8],
-            detection_boxes=detection_handler_pb2.float_array(numbers=[1, 2, 3, 4], shape=[1, 4]),
-            instance_name='testing',
-            frame=detection_handler_pb2.float_array(numbers=[1, 2, 3, 3, 4, 5], shape=[2,3]),
-            frame_count=1110,
-            source="code",
-            float_map={"frame_height": 400, "frame_width": 400}))
 
     # start flask
     app.debug = True
